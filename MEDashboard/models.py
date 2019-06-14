f8855d00319f37c57a98c4fc0ff4c2e7a3b6f7d3
GeoMotta <mottasgeorge@gmail.com>
2019-06-14 05:32:57 +0300

    initial database setup

---
+++ MEDashboard/models.py
@@ -0,0 +1,23 @@
+from MEDashboard import app, db
+from datetime import datetime
+
+from sqlalchemy import Table, Column, Integer, String, MetaData,ForeignKey
+
+
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.Text, nullable=False)
+    start_date = Column(db.DateTime, default = datetime.utcnow) 
+    description = db.Column(db.String(300))
+
+    def __repr__(self):
+        return "<Project '{}': '{}'>".format(self.name, self.description)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
