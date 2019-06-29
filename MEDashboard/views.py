354e1ac2f8d11ae51eab9d31d0a5837b02f6c28f
GeoMotta <mottasgeorge@gmail.com>
2019-06-29 20:58:15 +0300

    Saturday code

--- MEDashboard/views.py
+++ MEDashboard/views.py
@@ -22,3 +22,16 @@ def logIn():
 @app.route('/add_project')
 def addProject():
     return render_template('new_project.html')
+
+
+@app.route('/user_home')
+def userHome():
+    return render_template('user_home.html')
+
+@app.route('/new_user', methods=['POST', 'GET'])
+def addUser():
+    if request.method == 'POST':
+        username = request.form['username']
+        flash('Hallow world')
+        return redirect(url_for('userHome'))
+    return render_template('create_user.html')
